52ac2d4fc1c0eff19b1835ca96ca218e4cb526bf
Mohib Arshi <mohibarshi834@gmail.com>
2019-05-30 15:10:24 +0530

    --allow-empty-msg

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI:'mongodb+srv://webdev:MOHIB@mohib28@cluster0-ir48n.mongodb.net/test',
-    googleClientID:'228045048946-k7gb0m7lpt99d22pb74dndc95oogciud.apps.googleusercontent.com',
-    googleClientSecret:'03ToHSGe9oAOt5dpGs_SLKVW'
+    googleClientID:'228045048946-e54jqqko84dgn7jf12dio9n7falie7qh.apps.googleusercontent.com',
+    googleClientSecret:'ID87G7JVKLcQMSwwLoqfl_9b'
 }
